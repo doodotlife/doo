871078f58dd0a3788496531b5d633b52551e6e68
Jiacheng Jiang <ajax15601560911@gmail.com>
2016-11-26 03:30:31 -0500

    Finished log in page, working on fronted session

--- assets/scripts/login.js
+++ assets/scripts/login.js
@@ -1,7 +1,31 @@
+"use strict"
+
 $(document).ready(function() {
     // Stuff to do as soon as the DOM is ready
-    $('#login').submit(function(e) {
+    $('#loginButton').click(function(e) {
         e.preventDefault;
-        console.log("login");
+        let body={};
+        body.username = $("input[name=username]").val();
+        body.password = $("input[name=password]").val();
+        console.log(body);
+
+        $.ajax({
+            url: '/login',
+            type: 'POST',
+            dataType: "text",
+            contentType: "application/json; charset=utf-8",
+            data: JSON.stringify(body),
+            success: function (res) {
+                if (res == 'Login failed') {
+                    $("#hint").text("Username or password invalid. Please try again");
+                } else {
+                    window.location.replace("/")
+                }
+
+            },
+
+
+        });
+
     });
 });
