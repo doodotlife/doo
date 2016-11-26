692813b292cca7bcc302b1b935d391280c4bd1cd
Jiacheng Jiang <ajax15601560911@gmail.com>
2016-11-26 14:04:02 -0500

    Refined frontend

--- assets/scripts/login.js
+++ assets/scripts/login.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
     // Stuff to do as soon as the DOM is ready
     $('#loginButton').click(function(e) {
         e.preventDefault;
-        let body={};
+        let body = {};
         body.username = $("input[name=username]").val();
         body.password = $("input[name=password]").val();
         console.log(body);
@@ -15,17 +15,23 @@ $(document).ready(function() {
             dataType: "text",
             contentType: "application/json; charset=utf-8",
             data: JSON.stringify(body),
-            success: function (res) {
+            success: function(res) {
                 if (res == 'Login failed') {
                     $("#hint").text("Username or password invalid. Please try again");
+                    $("#hint").show();
                 } else {
                     window.location.replace("/")
                 }
-
             },
-
-
         });
+    });
 
+    $('#signUpButton').click(function(e) {
+        e.preventDefault;
+        $.ajax({
+            url: '/signup',
+            type: 'GET'
+        });
+        // $.get("/signup");
     });
 });
