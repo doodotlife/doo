4c6798abec96733085c1705df42022031e8d7999
Weixiang Zheng <zhengwz_2006@126.com>
2016-11-26 15:32:06 -0500

    Change event Owner to String Type

--- routes/routes.js
+++ routes/routes.js
@@ -127,7 +127,7 @@ module.exports = {
                     if (err) return res.send(500, {
                         error: err
                     });
-                    newEvent.owner = req.body.user;
+                    newEvent.owner = req.body.username;
                     newEvent.save();
                     return res.send("Success");
                 });
