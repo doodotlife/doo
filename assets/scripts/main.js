7c4bb7db57d4655a1d5b1b77418f7bb836b4dc60
Jiacheng Jiang <ajax15601560911@gmail.com>
2016-11-28 02:15:49 -0500

    fixed signup bug

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,16 +1,3 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
 $(document).ready(function() {
 
     $("#titleEntry").on("click", function() {
@@ -18,25 +5,47 @@ $(document).ready(function() {
         $("#titleEntry").prop("placeholder", "Title");
     });
 
-    $("#newEvent").submit( function(e) {
-        e.preventDefault();
-
-        let newEvent = {};
-        newEvent.title = $("#titleEntry").val();
-        newEvent.time = $("#timeEntry").val();
-        newEvent.type = $("input[name=type]:checked").val();
-        newEvent.private = $("input[name=private]:checked").val();
-        let newReq = {};
-        newReq.user = "5836a530597db1297c3894a2";
-        newReq.event = newEvent;
-        $.post('/event', newReq);
-        // location.reload(false);
-    });
+    // $("#newEvent").submit(function(e) {
+    //     e.preventDefault();
+    //
+    //     let newEvent = {};
+    //     newEvent.title = $("#titleEntry").val();
+    //     newEvent.time = $("#timeEntry").val();
+    //     newEvent.type = $("input[name=type]:checked").val();
+    //     newEvent.private = $("input[name=private]:checked").val();
+    //     let newReq = {};
+    //     newReq.user = "583907353f48805ef162143a";
+    //     newReq.event = newEvent;
+    //     $.post('/event', newReq);
+    //     // location.reload(false);
+    // });
 
     $("#cancelAdd").on("click", function() {
+        $("#titleEntry").prop("placeholder", "New Event");
         $("#addTable").removeClass("expandUp");
         $("#addTable").hide();
         // $("input").val('');
     });
-     // Stuff to do as soon as the DOM is ready
-});
+    // let events = $(".animate-opacity");
+    // for (var i = 0; i < events.length; i++) {
+    //     events[i].delay(i * 1000);
+    // }
+
+    // $(".topBar").on("scroll", function(e) {
+    //
+    //     if (this.scrollTop > 147) {
+    //         $(".topBar").addClass("fix-search");
+    //     } else {
+    //         $(".topBar").removeClass("fix-search");
+    //     }
+    // });
+        // $("button").click(function(){
+        //     $("#div1").delay("slow").fadeIn();
+        //     $("#div2").delay("fast").fadeIn();
+        //     $("#div3").delay(800).fadeIn();
+        //     $("#div4").delay(2000).fadeIn();
+        //     $("#div5").delay(4000).fadeIn();
+        //   });
+        // $.post('/events');
+        // Stuff to do as soon as the DOM is ready
+    });
