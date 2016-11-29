b58257ff48cb06b8503913070b832b7e48d2f461
Jiacheng Jiang <ajax15601560911@gmail.com>
2016-11-29 18:42:51 -0500

    Fixed single event page, refined style

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -4,11 +4,6 @@ showCommentBar = function(e) {
     e.on("click", hideCommentBar(this));
 }
 
-hideCommentBar = function(e) {
-    let id = e.closest(".event").id;
-    $("#" + id).children(".commentBar").css("height", "0");
-    e.on("click", showCommentBar(this));
-}
 
 $(document).ready(function() {
     // $(".commentBar").hide();
@@ -35,12 +30,28 @@ $(document).ready(function() {
         event.stopPropagation();
     });
 
-    $(".doComment").on("click", function() {
+    $(".doComment").on("click", function(e) {
         let id = this.closest(".event").id;
         $("#" + id).children(".commentBar").css({
             "height": "60",
             "border-top": "1px solid #efefef"
         });
+        $("#" + id).find(".doComment").addClass("closeComment");
+        $("#" + id).find(".doComment").removeClass("doComment");
+        // $("#" + id).children(".doComment").one('click', function() {
+        //     console.log("one click");
+        //     $("#" + id).children(".commentBar").css("height", "0");
+        // });
+    });
+
+    $(".closeComment").on("click", function(e) {
+        let id = this.closest(".event").id;
+        $("#" + id).children(".commentBar").css({
+            "height": "0",
+            "border-top": "1px solid #FFFFFF"
+        });
+        $("#" + id).find(".closeComment").addClass("doComment");
+        $("#" + id).find(".closeComment").removeClass("closeComment");
         // $("#" + id).children(".doComment").one('click', function() {
         //     console.log("one click");
         //     $("#" + id).children(".commentBar").css("height", "0");
@@ -58,50 +69,88 @@ $(document).ready(function() {
         });
     });
 
-// $("#newEvent").submit(function(e) {
-//     e.preventDefault();
-//
-//     let newEvent = {};
-//     newEvent.title = $("#titleEntry").val();
-//     newEvent.time = $("#timeEntry").val();
-//     newEvent.type = $("input[name=type]:checked").val();
-//     newEvent.private = $("input[name=private]:checked").val();
-//     let newReq = {};
-//     newReq.user = "583907353f48805ef162143a";
-//     newReq.event = newEvent;
-//     $.post('/event', newReq);
-//     // location.reload(false);
-// });
+    // $("#newEvent").submit(function(e) {
+    //     e.preventDefault();
+    //
+    //     let newEvent = {};
+    //     newEvent.title = $("#titleEntry").val();
+    //     newEvent.time = $("#timeEntry").val();
+    //     newEvent.type = $("input[name=type]:checked").val();
+    //     newEvent.private = $("input[name=private]:checked").val();
+    //     let newReq = {};
+    //     newReq.user = "583907353f48805ef162143a";
+    //     newReq.event = newEvent;
+    //     $.post('/event', newReq);
+    //     // location.reload(false);
+    // });
 
-$("#cancelAdd").on("click", function() {
+    $("#cancelAdd").on("click", function() {
 
-    $("#titleEntry").prop("placeholder", "New Event");
-    $("#feeds").css("margin-top", "10px");
-    $("#addTable").css({
-        "height": "0px",
-        "padding": "0 20"
+        $("#titleEntry").prop("placeholder", "New Event");
+        $("#feeds").css("margin-top", "10px");
+        $("#addTable").css({
+            "height": "0px",
+            "padding": "0 20"
+        });
     });
-});
-// let events = $(".animate-opacity");
-// for (var i = 0; i < events.length; i++) {
-//     events[i].delay(i * 1000);
-// }
 
-// $(".topBar").on("scroll", function(e) {
-//
-//     if (this.scrollTop > 147) {
-//         $(".topBar").addClass("fix-search");
-//     } else {
-//         $(".topBar").removeClass("fix-search");
-//     }
-// });
-// $("button").click(function(){
-//     $("#div1").delay("slow").fadeIn();
-//     $("#div2").delay("fast").fadeIn();
-//     $("#div3").delay(800).fadeIn();
-//     $("#div4").delay(2000).fadeIn();
-//     $("#div5").delay(4000).fadeIn();
-//   });
-// $.post('/events');
-// Stuff to do as soon as the DOM is ready
+    $(".commentEntry").on("submit", function(e) {
+        e.preventDefault();
+        let comment = {};
+        let id = this.closest(".event").id;
+        console.log("commenting event id: " + id);
+        comment.content = $("#" + id).find("input[name=content]").val()
+        comment.event = id;
+        $.ajax({
+            url: "/comment",
+            type: "POST",
+            dataType: "text",
+            contentType: "application/json; charset=utf-8",
+            data: JSON.stringify(comment),
+            // data:comment,
+            success: function(res) {
+
+                $("#" + id).find(".commentButtonText").html(res + " Comments");
+                $("#" + id).find("input[name=content]").val("");
+            }
+        });
+    });
+
+    $(".getEvent").on("click", function(e) {
+        let id = this.closest(".event").id;
+        $.ajax({
+            url: "/event?event=" + id,
+            type: "GET",
+            dataType: "html",
+            // contentType: "application/json; charset=utf-8",
+            // data:comment,
+            success: function(res) {
+                location.reload();
+            }
+        });
+        // $.get("/event?event=" + id);
+        //  window.reload("/event?event=" + id);
+    });
+    // let events = $(".animate-opacity");
+    // for (var i = 0; i < events.length; i++) {
+    //     events[i].delay(i * 1000);
+    // }
+
+    // $(".topBar").on("scroll", function(e) {
+    //
+    //     if (this.scrollTop > 147) {
+    //         $(".topBar").addClass("fix-search");
+    //     } else {
+    //         $(".topBar").removeClass("fix-search");
+    //     }
+    // });
+    // $("button").click(function(){
+    //     $("#div1").delay("slow").fadeIn();
+    //     $("#div2").delay("fast").fadeIn();
+    //     $("#div3").delay(800).fadeIn();
+    //     $("#div4").delay(2000).fadeIn();
+    //     $("#div5").delay(4000).fadeIn();
+    //   });
+    // $.post('/events');
+    // Stuff to do as soon as the DOM is ready
 });
