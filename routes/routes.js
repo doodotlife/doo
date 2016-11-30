56d9c685fdc37859d8741d649302e727a7551c45
Weixiang Zheng <zhengwz_2006@126.com>
2016-11-30 00:01:52 -0500

    Add decision condition and delete deleteEvents

--- routes/routes.js
+++ routes/routes.js
@@ -293,7 +293,8 @@ module.exports = {
         db.Event.findOne({
             "_id": req.body.event
         }, function(err, eventObj) {
-            if (req.user.username == eventObj.owner) {
+            if ((req.user.username == eventObj.owner)||
+            (req.user.adminPrivilege)) {
                 helper.deleteEventHelper(req.body.event);
                 res.send("success");
             } else {
@@ -689,10 +690,6 @@ module.exports = {
     /**Functions for Admin users**/
     deleteUsers: function() {
         // delete the selected users from database
-    },
-
-    deleteEvents: function() {
-        // delete events from database
     },
 
     /* req.body format
